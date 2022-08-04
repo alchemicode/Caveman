85f0d028e9d08506915537eee6abfb34e9194465
Michal <michal@tar.black>
2022-08-04 03:40:52 +0100

    New embed-ified ping command

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-
 import os
 
 from log import log
@@ -15,7 +14,7 @@ intents.members = True
 intents.message_content = True
 
 bot = commands.Bot(
-    command_prefix=commands.when_mentioned_or("_"), description=description, intents=intents
+    description=description, intents=intents
 )
 
 
@@ -24,14 +23,45 @@ async def on_ready():
     print(f"Logged in as {bot.user}, ID: {bot.user.id}")
 
 
+class Confirm(disnake.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.value = None
+
+    @disnake.ui.button(label="Press me!", style=disnake.ButtonStyle.green)
+    async def confirm(self, button: disnake.ui.Button, interaction: disnake.MessageInteraction):
+        embed = disnake.Embed(description="Confirmed!")
+        embed.set_footer(text="Bought to you with hopes and dreams")
+
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        self.value = True
+        self.stop()
+
+
 @bot.slash_command(
     name="ping",
     description="A simple Ping command",
 )
 async def ping(ctx):
+    embed = disnake.Embed(description="Pong!")
+    embed.set_footer(text="Bought to you with hopes and dreams")
+
+    view = Confirm()
+
     await ctx.response.send_message(
-        "Pong!", ephemeral=True
+        embed=embed,
+        view=view,
+        ephemeral=True,
     )
+
+    await view.wait()
+    if view.value is None:
+        print("Timed out")
+    elif view.value:
+        print("Confirmed")
+    else:
+        print("Cancelled")
+
 
 if __name__ == '__main__':
     log()
